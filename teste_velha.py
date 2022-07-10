55749a31cf41fb44cb6744bba66f36c85350e397
Vinícius Ribeiro <vgrgonzaga@outlook.com>
2022-07-10 15:59:12 -0300

    Teste de valor resultado indefinido ou não

--- teste_velha.py
+++ teste_velha.py
@@ -19,4 +19,13 @@ def test_right_value():
 def test_wrong_value():
   matrix_test = [(2,0,1),(6,2,1),(0,2,0)]
   assert run(matrix_test) == "Valor incorreto"
-  
+
+def test_not_undefined():
+  matrix_test = [(2,0,1),(2,0,0),(2,1,0)]
+  assert run(matrix_test) != "Resultado indefinido"
+
+def test_is_undefined():
+  matrix_test = [(2,0,1),(0,0,0),(0,2,0)]
+  assert run(matrix_test) == "Resultado indefinido"
+  matrix_test = [(2,0,1),(2,0,0),(2,1,0)]
+  assert run(matrix_test) == "Resultado indefinido"
