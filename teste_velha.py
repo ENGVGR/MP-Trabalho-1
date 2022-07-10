7a2d86aa6af1afeb1a60e8486801e78172475047
Vinícius Ribeiro <vgrgonzaga@outlook.com>
2022-07-10 15:51:37 -0300

    Execusao do pylint e correcao de erros de escrita

--- teste_velha.py
+++ teste_velha.py
@@ -1,17 +1,22 @@
+"""
+Tests to function in velha.py
+"""
+
 from velha import run
 
-def testRightLength():
-  matrixTest = [(0,0,0),(0,0,0),(0,0,0)]
-  assert run(matrixTest) != "Tamanho incorreto" 
+def test_right_length():
+  matrix_test = [(0,0,0),(0,0,0),(0,0,0)]
+  assert run(matrix_test) != "Tamanho incorreto"
 
-def testWrongLength():
-  matrixTest = [(0,0,0),(0,0,0)]
-  assert run(matrixTest) == "Tamanho incorreto" 
+def test_wrong_length():
+  matrix_test = [(0,0,0),(0,0,0)]
+  assert run(matrix_test) == "Tamanho incorreto"
 
-def testRightValue():
-  matrixTest = [(2,0,1),(0,2,1),(0,2,0)]
-  assert run(matrixTest) != "Valor incorreto" 
+def test_right_value():
+  matrix_test = [(2,0,1),(0,2,1),(0,2,0)]
+  assert run(matrix_test) != "Valor incorreto"
 
-def testWrongValue():
-  matrixTest = [(2,0,1),(6,2,1),(0,2,0)]
-  assert run(matrixTest) == "Valor incorreto" 
+def test_wrong_value():
+  matrix_test = [(2,0,1),(6,2,1),(0,2,0)]
+  assert run(matrix_test) == "Valor incorreto"
+  
--- velha.py
+++ velha.py
@@ -1,23 +1,25 @@
-def wrongLength(matrix):
-    if (len(matrix) != 3 or len(matrix[0]) != 3 or len(matrix[1]) != 3 or len(matrix[2]) != 3):
-        return True
-    else:
-        return False
+"""
+Functions to run a tic-tac-toe.
+"""
 
-def wrongValue(matrix):
-    correctValues = [0,1,2]
-    for line in matrix:
-        for value in line:
-            if (value not in correctValues):
-                return True
-    return False
+def wrong_length(matrix):
+  return (len(matrix) != 3 or len(matrix[0]) != 3 or len(matrix[1]) != 3
+    or len(matrix[2]) != 3)
+
+def wrong_value(matrix):
+  correct_values = [0,1,2]
+  for line in matrix:
+    for value in line:
+      if value not in correct_values:
+        return True
+  return False
 
 def run(matrix):
-    if (wrongLength(matrix)):
-        erro = "Tamanho incorreto"
-        return erro
-    if (wrongValue(matrix)):
-        erro = "Valor incorreto"
-        return erro
-    else: 
-        return 0
+  if wrong_length(matrix):
+    erro = "Tamanho incorreto"
+    return erro
+  if wrong_value(matrix):
+    erro = "Valor incorreto"
+    return erro
+  else:
+    return 0
