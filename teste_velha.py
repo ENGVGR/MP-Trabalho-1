b0acbc81da43f0a6d5aa1827a68c9c7b0a0ef5ee
Vinícius Ribeiro <vgrgonzaga@outlook.com>
2022-07-10 15:24:29 -0300

    Teste de valor correto e incorreto

--- teste_velha.py
+++ teste_velha.py
@@ -7,3 +7,11 @@ def testLengthRight():
 def testLengthWrong():
   matrixTest = [(0,0,0),(0,0,0)]
   assert run(matrixTest) == "Tamanho incorreto" 
+
+def testValueRight():
+  matrixTest = [(2,0,1),(0,2,1),(0,2,0)]
+  assert run(matrixTest) != "Valor incorreto" 
+
+def testValueWrong():
+  matrixTest = [(2,0,1),(6,2,1),(0,2,0)]
+  assert run(matrixTest) == "Valor incorreto" 
