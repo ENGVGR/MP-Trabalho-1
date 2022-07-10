d8bce1f0496df46c7e1795bc3b5192ebabe7ce89
Vinícius Ribeiro <vgrgonzaga@outlook.com>
2022-07-10 17:00:29 -0300

    Teste para resultados indefinidos

--- teste_velha.py
+++ teste_velha.py
@@ -22,11 +22,11 @@ def test_wrong_value():
 
 def test_not_impossible():
   matrix_test = [(2,0,1),(2,0,0),(2,1,0)]
-  assert run(matrix_test) != -1
+  assert run(matrix_test) != -2
 
 def test_is_impossible():
   matrix_test = [(2,0,1),(0,2,0),(0,2,0)]
-  assert run(matrix_test) == -1
+  assert run(matrix_test) == -2
 
 def test_is_winner():
   matrix_test = [(0,2,1),(0,2,1),(0,0,1)]
@@ -37,3 +37,11 @@ def test_is_winner():
 
   matrix_test = [(2,0,1),(1,2,0),(0,0,2)]
   assert run(matrix_test) == 2
+
+def test_not_undefined():
+  matrix_test = [(0,2,1),(0,2,1),(0,0,1)]
+  assert run(matrix_test) == 1
+
+def test_is_undefined():
+  matrix_test = [(0,0,0),(0,2,1),(0,0,1)]
+  assert run(matrix_test) == -1
